75767db077b72b8f723a4675ca1f069aca635ea7
AndypSheridan <andy.p.sheridan@gmail.com>
2022-08-11 10:07:33 +0000

    Add updated js code

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const toggleButton = document.getElementsByClassName('navbar-toggle')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links');
+let navOpen = false
+
 toggleButton.addEventListener('click', function() {
-    for(var i=0; i<navbarLinks.length; i++)
+    for(let i=0; i<navbarLinks.length; i++)
     navbarLinks[i].classList.toggle('active');
+    navOpen = true
 });
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 600 && navOpen) {
+        for(let i=0; i<navbarLinks.length; i++) {
+            if (navbarLinks[i].classList.contains('active')) {
+                navbarLinks[i].classList.toggle('active');
+            }
+        }
+        navOpen = false
+    }
+})
